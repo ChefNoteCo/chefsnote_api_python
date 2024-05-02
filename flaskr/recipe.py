c7d681fefb5ccdaf077a214661b00640484aa3c5
Patt Sookmark <pijiya@sookmark.com>
2024-05-02 11:57:08 -0600

    Remove learning files

--- flaskr/recipe.py
+++ flaskr/recipe.py
@@ -4,7 +4,6 @@ from flask import jsonify
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from flaskr.auth import login_required
 from flaskr.db import get_db
 import logging
 
