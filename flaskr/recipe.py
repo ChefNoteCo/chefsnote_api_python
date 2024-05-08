cdb7970bb9c5819338108c7e5d04ba19757348d1
Patt Sookmark <pijiya@sookmark.com>
2024-05-08 14:29:08 -0600

    Add route for adding new version

--- flaskr/recipe.py
+++ flaskr/recipe.py
@@ -14,11 +14,11 @@ logger = logging.getLogger(__name__)
 # Define blueprint
 bp = Blueprint('recipe', __name__, url_prefix='/recipe')
 
-# Define route to handle form submission
+#Add newly create recipe
 @bp.route('/record', methods=('POST',))
 def record():
     data = request.json
-
+    id = data.get('id')
     recipeName = data.get('recipeName')
     prepTime = data.get('prepTime')
     cookTime = data.get('cookTime')
@@ -26,19 +26,34 @@ def record():
     feedbackNotes = data.get('feedbackNotes')
     prepNotes = data.get('prepNotes')
     instruction = data.get('instruction')
+    parentId = data.get('parentId')
+    # check if the recipe from form is a newly create recipe
+    if id is None:
+        recipeVersionID = str(uuid.uuid4()) 
+    else:
+        recipeVersionID = parentId + "#" + str(date.today())
 
-    recipeVersionID = str(uuid.uuid4()) + "#" + str(date.today())
-    
+
     logger.info(f"Data - ID: {recipeVersionID}, Recipe Name: {recipeName}, Prep Time: {prepTime}, Cook Time: {cookTime}, Servings: {servings}")
     
     db = get_db()
+
+    # Insert the new recipe to recipe table
     db.execute(
         "INSERT INTO recipes (id, parent_id, child_id, recipe_name, prepTime, cookTime, servings, feedback_notes, prep_notes, instruction) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(recipeVersionID, recipeVersionID, recipeVersionID, recipeName, prepTime, cookTime, servings, feedbackNotes, prepNotes, instruction),
             )
     db.commit()
+    # Update the child_id of the parent
+    db.execute(
+        "UPDATE recipes set "
+    )
+
+
    
     return jsonify({'message': 'Recipe added successfully'}), 201
 
+
+#get all recipes
 @bp.route('/allrecipes', methods=('GET',)) 
 def all_recipes():
     db = get_db()
@@ -61,3 +76,42 @@ def all_recipes():
     
     # Return the list of recipes as JSON
     return jsonify(recipes=recipe_json)
+
+#Record a new version of the existing recipes  
+@bp.route('/versionrecord', methods=('POST',))
+def version_record():
+    data = request.json
+    id = data.get('id')
+    recipeName = data.get('recipeName')
+    prepTime = data.get('prepTime')
+    cookTime = data.get('cookTime')
+    servings = data.get('servings')
+    feedbackNotes = data.get('feedbackNotes')
+    prepNotes = data.get('prepNotes')
+    instruction = data.get('instruction')
+    
+    # Parse id to create a new version
+    try: 
+        parseVersionId = id.split('#')[0]
+    except IndexError:
+        parseVersionId = id
+    
+    recipeVersionID = parseVersionId + "#" + str(date.today())
+
+    logger.info(f"Data - ID: {recipeVersionID}, Recipe Name: {recipeName}, Prep Time: {prepTime}, Cook Time: {cookTime}, Servings: {servings}")
+    
+    db = get_db()
+       # Update the child_id of the parent
+    db.execute(
+        "UPDATE recipes SET child_id = ? where id = ?" , (id,id)
+    )
+    db.commit()
+    # Insert the new recipe to recipe table
+    db.execute(
+        "INSERT INTO recipes (id, parent_id, child_id, recipe_name, prepTime, cookTime, servings, feedback_notes, prep_notes, instruction) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(recipeVersionID, id, recipeVersionID, recipeName, prepTime, cookTime, servings, feedbackNotes, prepNotes, instruction),
+            )
+    db.commit()
+ 
+
+   
+    return jsonify({'message': 'New version of the recipe added successfully'}), 201
